3915c590762aa8887b0afe57596304dd718a341f
Karine de Lima Castro <kamirinelcastro@gmail.com>
2026-02-22 22:12:30 -0300

    estilização

--- js/data.js
+++ js/data.js
@@ -3,18 +3,24 @@ const products = [
     id: 1,
     name: "Cupcake de Chocolate",
     price: 8.50,
-    image: "https://via.placeholder.com/300x200?text=Chocolate"
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
   },
   {
     id: 2,
     name: "Cupcake de Morango",
     price: 9.00,
-    image: "https://via.placeholder.com/300x200?text=Morango"
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
   },
   {
     id: 3,
     name: "Cupcake de Baunilha",
     price: 10.00,
-    image: "https://via.placeholder.com/300x200?text=Baunilha"
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
+  },
+   {
+    id: 4,
+    name: "Cupcake de Red Velvet",
+    price: 11.50,
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
   }
 ];
--- js/render.js
+++ js/render.js
@@ -5,7 +5,7 @@ function createProductCard(product) {
       <img src="${product.image}">
       <h3>${product.name}</h3>
       <p>R$ ${product.price.toFixed(2)}</p>
-      <button class="btn btn-primary">Adicionar</button>
+      <button class="btn btn-secondary">Adicionar</button>
     </div>
   `;
 }
