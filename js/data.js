2b9c0816c03fa58fb51ca61ced4b06813f3e26c3
Karine de Lima Castro <kamirinelcastro@gmail.com>
2026-02-25 09:29:50 -0300

    products and cart

--- js/data.js
+++ js/data.js
@@ -22,5 +22,30 @@ const products = [
     name: "Cupcake de Red Velvet",
     price: 11.50,
     image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
-  }
+  },
+{
+    id: 5,
+    name: "Cupcake de Chocolate",
+    price: 8.50,
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
+  },
+  {
+    id: 6,
+    name: "Cupcake de Morango",
+    price: 9.00,
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
+  },
+  {
+    id: 7,
+    name: "Cupcake de Baunilha",
+    price: 10.00,
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
+  },
+   {
+    id: 8,
+    name: "Cupcake de Red Velvet",
+    price: 11.50,
+    image: "https://images.pexels.com/photos/461430/pexels-photo-461430.jpeg"
+  },
+
 ];
